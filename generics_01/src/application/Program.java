bba8f68df97200457cbb5bef066a902c62a08db5
Abrão Astério Junior <abcorinthians@gmail.com>
2021-04-13 21:46:26 -0300

    Demo 1 of Generics

--- generics_01/src/application/Program.java
+++ generics_01/src/application/Program.java
@@ -9,20 +9,20 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 
-		PrintService ps = new PrintService();
-		
+		PrintService<Integer> ps = new PrintService<>();
+
 		System.out.print("How many values? ");
 		int n = sc.nextInt();
-		
+
 		for (int i = 0; i < n; i++) {
 			Integer value = sc.nextInt();
 			ps.addValue(value);
 		}
 
 		ps.print();
-		Integer x = (Integer) ps.first();
+		Integer x = ps.first();
 		System.out.println("First: " + x);
-		
+
 		sc.close();
 	}
 }
