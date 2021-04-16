114ba3fe1177f5bc3fc580795e746564d83f0f37
Abrão Astério Junior <abcorinthians@gmail.com>
2021-04-16 03:29:55 -0300

    Demo 1 of Predicate: Standard implementation

---
+++ lambda_02/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import model.entities.Product;
+import util.ProductPredicate;
+
+public class Program {
+
+	/* Make a program that, from a list of products, removes from the list only
+	 * those whose minimum price is 100.*/
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		list.removeIf(new ProductPredicate());
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
