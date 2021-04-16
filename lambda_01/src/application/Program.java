b4a58293f2ad526b5b24d3996f2cec7a9506633a
Abrão Astério Junior <abcorinthians@gmail.com>
2021-04-15 23:49:40 -0300

    Demo 1 of Comparator: Standard Comparator

---
+++ lambda_01/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import model.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		list.sort(new MyComparator());
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
