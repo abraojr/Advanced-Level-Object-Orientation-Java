9765ef3fcd01716ff409d613d8bc8873987be1b6
Abrão Astério Junior <abcorinthians@gmail.com>
2021-04-16 03:52:01 -0300

    Demo 1 of Consumer: Interface implementation

---
+++ lambda_03/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import model.entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+
+	//Make a program that, from a list of products, increases the price of the products by 10%.
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		list.forEach(new PriceUpdate());
+
+		list.forEach(System.out::println);
+	}
+}
