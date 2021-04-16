bac1abbee148906c15ec0cd71d8dc8da5c37339f
Abrão Astério Junior <abcorinthians@gmail.com>
2021-04-16 04:16:27 -0300

    Demo 2 of Function: Method reference static

--- lambda_04/src/application/Program.java
+++ lambda_04/src/application/Program.java
@@ -6,7 +6,6 @@ import java.util.Locale;
 import java.util.stream.Collectors;
 
 import model.entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 
@@ -22,8 +21,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
-		
+		List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+
 		names.forEach(System.out::println);
 	}
 }
